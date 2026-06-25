e154d2564d7326613e94342e22cc78a0c6716746
bas6672 <bas6672ff@gmail.com>
2026-06-25 13:42:30 +0700

    frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Herosection from '@/components/herosection'
+import Cardsection from '@/components/Cardsection'
+import Footersection from '@/components/Footersection'
 
 export default function Home() {
   return (
-    <div><center>Home Page</center></div>
+    <div>
+    <Herosection />
+    <Cardsection />
+    <Footersection />
+    </div>
   )
 }
